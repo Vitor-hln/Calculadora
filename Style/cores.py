18d79fe46aa8705d5ee98002489e42ef607bead8
Vitor Hugo <vitor_hln@outlook.com>
2024-12-09 11:18:56 -0300

    Adicionando modularização e iniciando botões

---
+++ Style/cores.py
@@ -0,0 +1,7 @@
+cor1 = "#000000" # Black/Preto
+cor2 = "#f7f5f5" # White/Branco
+cor3 = "#d0d8db" # Grey/Cinza
+cor4 = "#d4820f" # Orange/Laranja
+cor5 = "#147ae0" # Azul
+
+fundo = "#202120" # Dark-light
--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-# Importa biblioteca tkinter
-from tkinter import *
+# Importações
+
+from tkinter import Tk, Frame, Button
 from tkinter import ttk
-
-#cores
-
-cor1 = "#000000" # Black/Preto
-cor2 = "#f7f5f5" # White/Branco
-cor3 = "#858c87" # Grey/Cinza
-cor4 = "#d4820f" # Orange/Laranja
-cor5 = "#0d50bd" # Azul
-
-fundo = "#202120" # Dark-light
+from Style import cores
 
 # Janela principal
 janela = Tk()
 janela.title("Calculadora") # Define o titulo da janela
-janela.geometry("300x400")  # Define largura e comprimento respectivamente
-janela.config(bg=fundo)
+janela.geometry("250x300")  # Define largura e comprimento respectivamente
+janela.config(bg = cores.fundo)
 
 # Frame - Display
-frame_tela = Frame(janela,width=300,height=50)
+frame_tela = Frame(janela,width=300,height=60,bg=cores.cor5)
 frame_tela.grid(row=0, column=0) # posição do frame
 
-# Frame - botões
-frame_corpo = Frame(janela,width=300,height=350)
+frame_corpo = Frame(janela,width=300,height=350,bg=cores.fundo)
 frame_corpo.grid(row=1, column=0) # posição do frame
+
+# Create Widgets
+b1 = Button(frame_corpo, text="C", width=11, height=2,bg=cores.cor3)
+b1.place(x=0,y=0)
+b2 = Button(frame_corpo, text="%",width=5, height=2,bg=cores.cor3)
+b2.place(x=87,y=0)
+b3 = Button(frame_corpo, text=)
+b4 = Button()
+b5 = Button()
+b6 = Button()
+b7 = Button()
+b8 = Button()
+b9 = Button()
+b10 = Button()
+
 
 janela.mainloop() # Executa a janela
